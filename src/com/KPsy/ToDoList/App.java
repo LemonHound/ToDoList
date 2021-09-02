6aed3a8f5f61494f42dc68880a5f9bdb60746535
LemonHound <seiwertkevin@gmail.com>
2021-09-01 20:28:48 -0500

    Basic Functionality Implementation with some test statements

--- src/com/KPsy/ToDoList/App.java
+++ src/com/KPsy/ToDoList/App.java
@@ -22,18 +22,25 @@ public class App extends JFrame{
     private void initUI() {
         add(new ToDoList());
 
+        GridBagLayout gridBagLayout = new GridBagLayout();
+        setLayout(gridBagLayout);
         setResizable(true);
         pack();
 
         setTitle("To Do List");
-        setLocationRelativeTo(null);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int width = (int) screenSize.getWidth();
+        int height = (int) screenSize.getHeight();
+        setLocation(50,50);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
+
 
     public static void main(String[] args) {
         EventQueue.invokeLater(() -> {
 
             var ex = new App();
+            ex.setSize(new Dimension(800,600));
             ex.setVisible(true);
         });
     }
--- src/com/KPsy/ToDoList/ToDoList.java
+++ src/com/KPsy/ToDoList/ToDoList.java
@@ -1,31 +1,59 @@
 package com.KPsy.ToDoList;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
 
 public class ToDoList extends JPanel {
 
     // Variables
-
+    JTextPane testTextPane = new JTextPane();
+    JButton testButton = new JButton();
+    GridBagConstraints c = new GridBagConstraints();
 
     // Constructor
     public ToDoList(){
 
         generateUI();
-        loadToDoListFromSQL();
-        loadToDoListToUI();
+        loadToDoList();
+        displayToDoList();
 
+
+        testButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                    saveTodoList();
+            }
+        });
 
     }
 
     private void generateUI() {
+
+        testTextPane.setSize(200,100);
+
+        c.gridx = 0;
+        c.gridy = 0;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        add(testTextPane,c);
+        c.gridx = 1;
+        add(testButton,c);
+
     }
 
-    private void loadToDoListFromSQL(){
+    private void loadToDoList(){
 
     }
 
-    private void loadToDoListToUI(){
+    private void displayToDoList(){
 
+    }
+
+    private void saveTodoList(){
+        String s = testTextPane.getText();
+        System.out.println(s);
     }
 
 }
